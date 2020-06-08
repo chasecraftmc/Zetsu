b4323184ed9b69422f5303f15cfa373bb2fbe265
BlazingTide <blazingtide@gmail.com>
2020-06-08 11:16:14 -0700

    Fix issue where no parameter commands weren't working

--- src/main/java/me/blazingtide/zetsu/processor/impl/SpigotProcessor.java
+++ src/main/java/me/blazingtide/zetsu/processor/impl/SpigotProcessor.java
@@ -25,7 +25,9 @@ public class SpigotProcessor extends CommandProcessor implements CommandExecutor
         }
 
         for (int i = 0; i < found.getArgs().size(); i++) {
-            args = (String[]) ArrayUtils.remove(args, i);
+            if (args.length > i) {
+                args = (String[]) ArrayUtils.remove(args, i);
+            }
         }
 
         this.invoke(found, args, sender);
