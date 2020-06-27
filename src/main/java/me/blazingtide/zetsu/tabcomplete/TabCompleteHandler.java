a679b1d8ac2a595cdeca26ccf7ac941b52f9c524
BlazingTide <blazingtide@gmail.com>
2020-06-26 20:47:21 -0700

    begin work on tabcomplete

---
+++ src/main/java/me/blazingtide/zetsu/tabcomplete/TabCompleteHandler.java
@@ -0,0 +1,13 @@
+package me.blazingtide.zetsu.tabcomplete;
+
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+public class TabCompleteHandler {
+
+    public Set<String> request(String command) {
+        return Sets.newHashSet();
+    }
+
+}
