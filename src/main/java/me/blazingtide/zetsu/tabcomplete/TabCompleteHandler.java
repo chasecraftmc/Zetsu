73a2ab75aaa92fbea7a1bc596263a82f34993fe0
BlazingTide <blazingtide@gmail.com>
2020-06-26 21:03:46 -0700

    work on subcommand tab complete

--- src/main/java/me/blazingtide/zetsu/tabcomplete/TabCompleteHandler.java
+++ src/main/java/me/blazingtide/zetsu/tabcomplete/TabCompleteHandler.java
@@ -1,17 +1,22 @@
 package me.blazingtide.zetsu.tabcomplete;
 
-import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import lombok.AllArgsConstructor;
 import me.blazingtide.zetsu.Zetsu;
 import me.blazingtide.zetsu.schema.CachedCommand;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @AllArgsConstructor
 public class TabCompleteHandler {
+
+    private final Map<String, List<String>> subcommandCache = Maps.newHashMap(); //Instead of looping every time, we store the sub commands and loop ONCE. (under the impression sub commands won't be created mid runtime)
 
     private final Zetsu zetsu;
 
@@ -20,12 +25,26 @@ public class TabCompleteHandler {
     }
 
     public List<String> requestSubcommands(String label) {
-        return zetsu.getLabelMap().getOrDefault(label, Lists.newArrayList()).stream().map(new Function<CachedCommand, String>() {
-            @Override
-            public String apply(CachedCommand cachedCommand) {
-                return cachedCommand.getLabel().replace(label, "");
+        //very slow because if we have ~100 commands registered, it's going to do a loop every tab complete = lags the server...
+        // even tho it's barely noticeable, it will add up with a lot of players
+
+//        return zetsu.getLabelMap().getOrDefault(label, Lists.newArrayList()).stream().map(new Function<CachedCommand, String>() {
+//            @Override
+//            public String apply(CachedCommand cachedCommand) {
+//                return cachedCommand.getLabel().replace(label, "");
+//            }
+//        });
+
+        subcommandCache.computeIfAbsent(label, string -> zetsu.getLabelMap().get(string).stream().map(command -> {
+            final String[] split = command.getLabel().split(Zetsu.CMD_SPLITTER);
+            if (split.length == 0) {
+                return null;
             }
-        });
+
+            return split[split.length - 1];
+        }).filter(Objects::nonNull).collect(Collectors.toList()));
+
+        return subcommandCache.get(label);
     }
 
 }
